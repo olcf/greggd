6943a7ce056729a71f39d779edb8435fc6b468bd
Joseph Voss <vossjm@ornl.gov>
2019-12-06 14:31:19 -0500

    Use a ticker and add outType

--- tracer/sendOutput.go
+++ tracer/sendOutput.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"reflect"
+	"time"
 
 	"github.com/olcf/greggd/config"
 	bcc "github.com/josephvoss/gobpf/bcc"
@@ -24,13 +25,42 @@ func readPerfChannel(ctx context.Context, outType reflect.Type,
 			tags, fields := make(map[string]string), make(map[string]string)
 			outputChan <- config.SocketInput{
 				MeasurementName: mapName, Fields: fields, Tags: tags,
-				Bytes: inputBytes, Output: output,
+				Bytes: inputBytes, Output: output, Type: outType,
 			}
 		}
 	}
 }
 
 func iterateHashMap(ctx context.Context, table *bcc.Table,
+	outType reflect.Type, socketChan chan config.SocketInput, errChan chan error,
+	output *config.BPFOutput, globals config.GlobalOptions) {
+
+	sleepDuration, err := time.ParseDuration(output.Poll)
+	if err != nil {
+		errChan <- fmt.Errorf("tracer.go: Error parsing poll time %s: %s\n",
+			output.Poll, err)
+		return
+	}
+
+	ticker := time.NewTicker(sleepDuration)
+	defer ticker.Stop()
+
+	// Infinite loop, call loopHashMap every polling period
+	loopHashMap(ctx, table, outType, socketChan, errChan, output, globals)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Done")
+			return
+		case <-ticker.C:
+			loopHashMap(ctx, table, outType, socketChan, errChan, output, globals)
+		}
+	}
+
+	return
+}
+
+func loopHashMap(ctx context.Context, table *bcc.Table,
 	outType reflect.Type, socketChan chan config.SocketInput, errChan chan error,
 	output *config.BPFOutput, globals config.GlobalOptions) {
 
@@ -68,12 +98,10 @@ func iterateHashMap(ctx context.Context, table *bcc.Table,
 			binary.LittleEndian.Uint64(tableIter.Key()))}
 
 		// Write data to struct and send it on
+		fmt.Println("Writing to socket")
 		socketChan <- config.SocketInput{
 			MeasurementName: table.ID(), Fields: fields, Tags: map[string]string{},
-			Bytes: val, Output: output,
+			Bytes: val, Output: output, Type: outType,
 		}
 	}
-
-	return
-
 }
