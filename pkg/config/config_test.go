8248bbab60fc51e83eeae0dec5bda15b3ccff559
Joseph Voss <vossjm@ornl.gov>
2020-03-10 12:18:40 -0400

    Add retryDelay compilation tests

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -70,3 +70,25 @@ programs: [{source: fake, outputs: [type: fake]}]
 		t.Errorf("Default value for program key type not set")
 	}
 }
+
+// Confirm time compliation fails with bad value
+func TestParseConfigRetryTimeCompileErr(t *testing.T) {
+	emptyConfig := strings.NewReader(`globals: {retryDelay: fake}`)
+	_, err := ParseConfig(emptyConfig)
+	if err == nil {
+		t.Errorf("Invalid retry time did not throw error: %v", err)
+	}
+}
+
+// Confirm time compliation returns valid time object
+func TestParseConfigRetryTimeCompile(t *testing.T) {
+	emptyConfig := strings.NewReader(``)
+	testConfig, err := ParseConfig(emptyConfig)
+	if err != nil {
+		t.Errorf("Error thrown when not expected: %v", err)
+		return
+	}
+	if testConfig.Globals.CompiledRetryDelay.String() == "" {
+		t.Errorf("Compiled retryDelay is not populated time.duration object")
+	}
+}
