224e74246ec8bbe66f14a55c9259dcb45bb3e4d6
Joseph Voss <vossjm@ornl.gov>
2020-05-08 11:05:05 -0400

    Adds test for build struct from array

---
+++ pkg/communication/binary_test.go
@@ -0,0 +1,38 @@
+package communication
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/olcf/greggd/pkg/config"
+)
+
+func TestBuildStructFromArray(t *testing.T) {
+	expected := reflect.StructOf([]reflect.StructField{
+		reflect.StructField{Name: strings.Title("id"),
+			Type: reflect.TypeOf(uint64(0))},
+		reflect.StructField{Name: strings.Title("pid"),
+			Type: reflect.TypeOf(uint32(0))},
+		reflect.StructField{Name: strings.Title("uid"),
+			Type: reflect.TypeOf(uint32(0))},
+		reflect.StructField{Name: strings.Title("ret"),
+			Type: reflect.TypeOf(int32(0))},
+		reflect.StructField{Name: strings.Title("comm"),
+			Type: reflect.ArrayOf(16, reflect.TypeOf(byte(0)))},
+	})
+	input := []config.BPFOutputFormat{
+		config.BPFOutputFormat{Name: "id", Type: "u64"},
+		config.BPFOutputFormat{Name: "pid", Type: "u32"},
+		config.BPFOutputFormat{Name: "uid", Type: "u32"},
+		config.BPFOutputFormat{Name: "ret", Type: "int32"},
+		config.BPFOutputFormat{Name: "comm", Type: "char[16]"},
+	}
+	actual, err := BuildStructFromArray(input)
+	if err != nil {
+		t.Errorf("Error got trying to build struct: %v", err)
+	}
+	if expected != actual {
+		t.Errorf("Output of BuildStructFromArray doesn't equal expected.")
+	}
+}
