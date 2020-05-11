6cf286f2cffddb1ec5a8ff5e182cfadcd1966ca0
Joseph Voss <vossjm@ornl.gov>
2020-05-11 15:13:44 -0400

    Adds test to convert integers from binary arrays

--- pkg/communication/binary_test.go
+++ pkg/communication/binary_test.go
@@ -36,3 +36,54 @@ func TestBuildStructFromArray(t *testing.T) {
 		t.Errorf("Output of BuildStructFromArray doesn't equal expected.")
 	}
 }
+
+func TestWriteBinaryToStruct(t *testing.T) {
+	tables := []struct {
+		inType      reflect.Type
+		inBytes     []byte
+		expectedVal reflect.Value
+		setVal      interface{}
+	}{
+		// Test if it can parse 56
+		{
+			reflect.TypeOf(int8(0)),
+			[]byte{56},
+			reflect.New(reflect.TypeOf(int8(0))).Elem(),
+			int8(56),
+		},
+		// Test if it can parse 100
+		{
+			reflect.TypeOf(int64(0)),
+			[]byte{100, 0, 0, 0, 0, 0, 0, 0, 0},
+			reflect.New(reflect.TypeOf(int64(0))).Elem(),
+			int64(100),
+		},
+		// Test if it can parse 123456
+		{
+			reflect.TypeOf(uint64(0)),
+			[]byte{64, 226, 1, 0, 0, 0, 0, 0},
+			reflect.New(reflect.TypeOf(uint64(0))).Elem(),
+			uint64(123456),
+		},
+		// Test if it can parse -153
+		{
+			reflect.TypeOf(int64(0)),
+			[]byte{256 - 153, 255, 255, 255, 255, 255, 255, 255, 255},
+			reflect.New(reflect.TypeOf(int64(0))).Elem(),
+			int64(-153),
+		},
+	}
+
+	for _, tbl := range tables {
+		tbl.expectedVal.Set(reflect.ValueOf(tbl.setVal))
+
+		outVal, err := writeBinaryToStruct(tbl.inBytes, tbl.inType)
+		if err != nil {
+			t.Errorf("Error got trying to write binary: %v", err)
+		}
+		if outVal.Interface() != tbl.expectedVal.Interface() {
+			t.Errorf("Output value doesn't match expected from binary: %+v != %+v",
+				outVal.Interface(), tbl.expectedVal.Interface())
+		}
+	}
+}
